8dec045dd45a15c49a6249fad5b87392ca75fcca
Shuvam Ghosh <shuvamghosh375@gmail.com>
2024-12-20 19:47:13 +0530

    add  register controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,8 @@
 import { Router } from "express";
+import { registerUser } from "../controllers/user.controller.js"; // Adjust path if needed
 
-const router = Router()
+const router = Router();
 
-router.route("/register").post(registerUser)
+router.route("/register").post(registerUser);
 
-export default router
-
-
+export default router;
